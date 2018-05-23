e2b13db1795a92950ae18c9fc5afc83cf3998f7e
dmutreja <dhiraj.mutreja@oracle.com>
2018-05-23 14:14:16 -0700

    Don't install cloud-init package

--- stemcell_builder/stages/system_oracle/apply.sh
+++ stemcell_builder/stages/system_oracle/apply.sh
@@ -9,7 +9,7 @@ source $base_dir/lib/prelude_apply.bash
 pkg_mgr purge grub grub-pc grub-common
 
 # Install v2 grub and efi
-pkg_mgr install cloud-init efibootmgr dmidecode ssh-import-id open-iscsi iptables-persistent \
+pkg_mgr install efibootmgr dmidecode ssh-import-id open-iscsi iptables-persistent \
   grub2-common grub-efi-amd64-bin lsscsi
 
 # https://bugs.launchpad.net/ubuntu/+source/grub2/+bug/1692235
